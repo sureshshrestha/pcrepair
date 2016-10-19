d96e2c006ab74a8cba680f496556d557d1e1d612
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-10-19 16:06:59 +0545

    3.4 Fetching JSON.

--- pcrepair/app.js
+++ pcrepair/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var about = require('./routes/about');
 var users = require('./routes/users');
+var services = require('./routes/services');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/about', about);
+app.use('/services', services);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
